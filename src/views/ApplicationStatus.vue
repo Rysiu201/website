3c5b1501d8c597ee72b20aea1aac9604a520457d
Rysiu201 <54910862+Rysiu201@users.noreply.github.com>
2025-06-15 00:15:04 +0200

    Update ApplicationStatus.vue

--- src/views/ApplicationStatus.vue
+++ src/views/ApplicationStatus.vue
@@ -1,8 +1,9 @@
 <template>
   <main class="status-page">
-    <h1>Twoje podanie zostało wysłane: <span :class="statusClass">{{ status }}</span></h1>
+    <h1>Twoje podanie zostało Wysłane</h1>
+    <h1>Obecny status twojego zgłoszenia: <b><span :class="statusClass">{{ status }}</span></b></h1>
     <p v-if="status === statuses.APPROVED" class="approved-msg">
-      Twoje podanie zostało rozpatrzone pozytywnie.
+      Twoje podanie zostało rozpatrzone {{ status }}.
       <a :href="discordLink" target="_blank">Dołącz na Discorda</a>
       i zgłoś się w celu dalszej rekrutacji.
     </p>
@@ -16,8 +17,8 @@ const statuses = {
   SENT: 'Wysłane',
   PENDING: 'Przyjęte, oczekuje na rozpatrzenie',
   IN_REVIEW: 'W trakcie rozpatrywania',
-  APPROVED: 'Rozpatrzone Pozytywnie',
-  REJECTED: 'Rozpatrzone negatywnie (Napisz nowe podanie w ciągu 24/48h)'
+  APPROVED: 'Pozytywnie',
+  REJECTED: 'Negatywnie (Napisz nowe podanie w ciągu 24/48h)'
 }
 
 const status = ref('')
