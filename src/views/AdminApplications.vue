cae51cc7055fbfdf2f18ec84a99b8ce4908be3e9
Rysiu201 <choroba000@icloud.com>
2025-06-16 01:32:47 +0200

    merge

--- src/views/AdminApplications.vue
+++ src/views/AdminApplications.vue
@@ -57,7 +57,7 @@ const statuses = {
   PENDING: 'Przyjęte, oczekuje na rozpatrzenie',
   IN_REVIEW: 'W trakcie rozpatrywania',
   APPROVED: 'Pozytywnie',
-  REJECTED: 'Negatywnie (Napisz nowe podanie w ciągu 24/48h)'
+  REJECTED: 'Negatywnie'
 }
 
 const columns: { key: keyof typeof statuses; label: string }[] = [
